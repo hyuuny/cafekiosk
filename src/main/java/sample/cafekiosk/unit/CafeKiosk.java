b91f9fa4e2d9ec67a54dd5ea911cbafa05d5e645
hyuuny <shyune@knou.ac.kr>
2024-04-04 20:36:34 +0900

    feat: TDD 방식으로 calculateTotalPrice 기능 구현(REFACTORING 2)

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -31,11 +31,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public void remove(Beverage beverage) {
