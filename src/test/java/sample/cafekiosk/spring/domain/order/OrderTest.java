7fb2a538242a2d860012026f414d4131aac660f8
hyuuny <shyune@knou.ac.kr>
2024-04-07 11:18:17 +0900

    feat: 주문 생성 시 registeredDateTime을 외부에서 받도록 구현

--- src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -1,11 +1,11 @@
 package sample.cafekiosk.spring.domain.order;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,10 +23,43 @@ class OrderTest {
         );
 
         // when
-        Order order = Order.create(product);
+        Order order = Order.create(product, LocalDateTime.now());
 
         // then
         assertThat(order.getTotalPrice()).isEqualTo(3000);
+    }
+
+    @DisplayName("주문 생성 시 주문 상태는 INIT이다.")
+    @Test
+    void init() {
+        // given
+        List<Product> product = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(product, LocalDateTime.now());
+
+        // then
+        assertThat(order.getOrderStatus()).isEqualByComparingTo(OrderStatus.INIT);
+    }
+
+    @DisplayName("주문 생성 시 등록 시간을 기록한다.")
+    @Test
+    void registeredDateTime() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        List<Product> product = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(product, registeredDateTime);
+
+        // then
+        assertThat(order.getRegisteredDateTime()).isEqualTo(registeredDateTime);
     }
 
     private Product createProduct(String productNumber, int price) {
