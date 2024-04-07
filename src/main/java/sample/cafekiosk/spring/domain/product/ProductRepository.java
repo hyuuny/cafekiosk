0e730a415c3612c3ced28aa0ff274812862ea816
hyuuny <shyune@knou.ac.kr>
2024-04-07 11:04:33 +0900

    feat: ProductRepository 테스트 작성
    -findAllByProductNumberIn

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -13,4 +13,11 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      */
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
 
+    /**
+     * select *
+     * from product
+     * where product_numbers in ();
+     */
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
 }
