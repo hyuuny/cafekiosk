e9c3f4e610d0f717b1e7a3c98e90ac128d2229d8
hyuuny <shyune@knou.ac.kr>
2024-04-07 17:31:39 +0900

    feat: 신규 주문을 등록한다.

---
+++ src/test/java/sample/cafekiosk/spring/api/controller/order/OrderControllerTest.java
@@ -0,0 +1,54 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.OrderService;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private OrderService orderService;
+
+    @DisplayName("신규 주문을 등록한다.")
+    @Test
+    void createOrder() throws Exception {
+        // given
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001"))
+                .build();
+
+        // when // then
+        mockMvc.perform(post("/api/v1/orders/new")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.name()))
+                .andExpect(jsonPath("$.message").value(HttpStatus.OK.name()));
+    }
+
+}
