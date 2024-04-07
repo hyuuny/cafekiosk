ef5bb8b4cf0b776aa5afd6f3f487acfd7b5eb94a
hyuuny <shyune@knou.ac.kr>
2024-04-07 15:50:15 +0900

    feat: 상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다(RED)

--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -47,6 +47,26 @@ class ProductServiceTest {
         assertThat(productResponse)
                 .extracting("productNumber", "type", "sellingStatus", "name", "price")
                 .contains("002", HANDMADE, SELLING, "카푸치노", 5000);
+    }
+
+    @DisplayName("상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다.")
+    @Test
+    void createProductWhenProductIsEmpty() {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("카푸치노")
+                .price(5000)
+                .build();
+
+        // when
+        ProductResponse productResponse = productService.createProduct(request);
+
+        // then
+        assertThat(productResponse)
+                .extracting("productNumber", "type", "sellingStatus", "name", "price")
+                .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
 
     }
 
