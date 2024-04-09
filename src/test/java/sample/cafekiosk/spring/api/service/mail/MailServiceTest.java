7b5bcee2096b8c3ecbaf1ccd5d51b412556b7e38
hyuuny <shyune@knou.ac.kr>
2024-04-09 21:05:24 +0900

    feat: InjectMocks을 사용하여, 스프링 의존성 없이 메일 전송 테스트 작성

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
@@ -23,12 +24,13 @@ class MailServiceTest {
     @Mock
     private MailSendHistoryRepository mailSendHistoryRepository;
 
+    @InjectMocks
+    private MailService mailService;
+
     @DisplayName("@Mock을 사용하여, 메일 전송 테스트")
     @Test
     void sendMail() {
         // given
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString())).thenReturn(true);
 
         // when
