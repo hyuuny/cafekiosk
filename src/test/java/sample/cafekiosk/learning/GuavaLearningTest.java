be6bb4221b722d15088e5456164d7db4eb541dbf
hyuuny <shyune@knou.ac.kr>
2024-04-12 21:22:06 +0900

    feat: 멀티맵(하나의 key에 여러 value) 삭제 기능 확인

--- src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
+++ src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
@@ -4,7 +4,9 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 import java.util.Collection;
 import java.util.List;
@@ -65,5 +67,38 @@ public class GuavaLearningTest {
         assertThat(coffee).hasSize(3)
                 .isEqualTo(List.of("아메리카노", "카페라떼", "카푸치노"));
     }
+
+    @DisplayName("멀티맵(하나의 key에 여러 value) 삭제 기능 확인")
+    @TestFactory
+    Collection<DynamicTest> multiMapDeleteLearningTest() {
+        // given
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("커피", "아메리카노");
+        multimap.put("커피", "카페라떼");
+        multimap.put("커피", "카푸치노");
+        multimap.put("베이커리", "크루아상");
+        multimap.put("베이커리", "식빵");
+
+        return List.of(
+                DynamicTest.dynamicTest("1개 value 삭제", () -> {
+                    // when
+                    multimap.remove("커피", "카푸치노");
+
+                    // then
+                    Collection<String> coffee = multimap.get("커피");
+                    assertThat(coffee).hasSize(2)
+                            .isEqualTo(List.of("아메리카노", "카페라떼"));
+                }),
+                DynamicTest.dynamicTest("1개 key 삭제", () -> {
+                    // when
+                    multimap.removeAll("커피");
+
+                    // then
+                    Collection<String> coffee = multimap.get("커피");
+                    assertThat(coffee).isEmpty();
+                })
+        );
+    }
+
 
 }
