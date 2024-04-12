1805eeca8b8dc3de484fc089079f48c0e4491134
hyuuny <shyune@knou.ac.kr>
2024-04-12 21:18:30 +0900

    feat: 멀티맵(하나의 key에 여러 value) 기능 확인

--- src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
+++ src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
@@ -1,9 +1,12 @@
 package sample.cafekiosk.learning;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -42,6 +45,25 @@ public class GuavaLearningTest {
                         List.of(1, 2, 3, 4),
                         List.of(5, 6)
                 ));
+    }
+
+    @DisplayName("멀티맵(하나의 key에 여러 value) 기능 확인")
+    @Test
+    void multiMapLearningTest() {
+        // given
+        Multimap<String, String> multimap = ArrayListMultimap.create();
+        multimap.put("커피", "아메리카노");
+        multimap.put("커피", "카페라떼");
+        multimap.put("커피", "카푸치노");
+        multimap.put("베이커리", "크루아상");
+        multimap.put("베이커리", "식빵");
+
+        // when
+        Collection<String> coffee = multimap.get("커피");
+
+        // then
+        assertThat(coffee).hasSize(3)
+                .isEqualTo(List.of("아메리카노", "카페라떼", "카푸치노"));
     }
 
 }
